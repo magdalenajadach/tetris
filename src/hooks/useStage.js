158f229a60646814f749d22a3255cd1ed5c9f1a0
Magdalena Jadach <magdalena@raspberrypi.org>
2024-01-11 20:33:03 +0000

    Stage - random tetromino

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createStage } from "../helpers/createStageHelper";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // clean the stage
+      const newStage = prevStage.map((row) =>
+        row.map((square) => (square[1] === "clear" ? [0, "clear"] : square))
+      );
+      // draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.position.y][x + player.position.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+      return newStage;
+    };
+    setStage((prev) => updateStage(prev));
+  }, [player.collided, player.position.x, player.position.y, player.tetromino]);
+
   return [stage, setStage];
 };
